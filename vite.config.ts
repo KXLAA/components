9b8f5b83b19e3e9d7492348e7d70f392c6725329
KXLAA <kolade.afode@yahoo.com>
2023-02-02 19:56:39 +0100

    fix: more config fixes
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,23 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import { resolve } from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig((configEnv) => {
-  const isDevelopment = configEnv.mode === "development";
+  const isDevelopment = configEnv.mode === 'development'
 
   return {
     plugins: [react()],
     resolve: {
       alias: [
-        { find: "@/", replacement: "/src" },
-        { find: "@/assets", replacement: "/src/assets" },
-        { find: "@/components", replacement: "/src/components" },
-      ],
+        { find: '@/', replacement: '/src' },
+        { find: '@/assets', replacement: '/src/assets' },
+        { find: '@/components', replacement: '/src/components' }
+      ]
     },
     css: {
       modules: {
-        generateScopedName: isDevelopment
-          ? "[name]__[local]__[hash:base64:5]"
-          : "[hash:base64:5]",
-      },
-    },
-  };
-});
+        generateScopedName: isDevelopment ? '[name]__[local]__[hash:base64:5]' : '[hash:base64:5]'
+      }
+    }
+  }
+})
